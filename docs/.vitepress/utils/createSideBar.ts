ae60ff635b0bf2a1aec2d24d9665de52934fbe6b
Jun <huanggaojun13@gmail.com>
2024-06-28 23:37:06 +0800

    add 6-28 ai daily

--- docs/.vitepress/utils/createSideBar.ts
+++ docs/.vitepress/utils/createSideBar.ts
@@ -4,7 +4,8 @@ export function createSideBarZH() {
       text: '2024年',
       collapsed: true,
       items: [
-        { text: '6 月 27 日', link: '/posts/2024/6-26' },
+        { text: '6 月 28 日', link: '/posts/2024/6-28' },
+        { text: '6 月 27 日', link: '/posts/2024/6-27' },
         { text: '6 月 26 日', link: '/posts/2024/6-26' },
         { text: '6 月 25 日', link: '/posts/2024/6-25' },
       ]
