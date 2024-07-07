6ada7c47a3bb14c55e19901ab2f23dc825b66394
Jun <huanggaojun13@gmail.com>
2024-07-07 22:37:45 +0800

    update 7-07 ai daily

--- docs/.vitepress/utils/createSideBar.ts
+++ docs/.vitepress/utils/createSideBar.ts
@@ -4,6 +4,7 @@ export function createSideBarZH() {
       text: '2024年',
       collapsed: true,
       items: [
+        { text: '7 月 7 日', link: '/posts/2024/7-07' },
         { text: '7 月 6 日', link: '/posts/2024/7-06' },
         { text: '7 月 5 日', link: '/posts/2024/7-05' },
         { text: '7 月 4 日', link: '/posts/2024/7-04' },
@@ -35,21 +36,3 @@ export function createSideBarEN() {
     }
   ]
 }
-
-分享一个快速、易用且高度可配置的终端 Spotify 播放器 spotify_player。
-
-具有如下功能：
-
-- 极简的用户界面，具有直观的分页和弹出系统。
-- 高度可配置，满足个性化需求。
-- 功能与官方 Spotify 应用程序相同。
-- 支持通过 Spotify Connect 进行远程控制。
-- 支持直接在终端中流式传输歌曲。
-- 支持大多数歌曲的歌词显示。
-- 支持在多个操作系统上进行媒体控制。
-- 支持专辑封面等图像的渲染。
-- 支持桌面通知。
-- 支持将应用程序作为守护进程运行。
-- 提供广泛的命令行接口 (CLI) 命令。
-
-这播放器装在电脑上，黑客感瞬间就上来了，被朋友看到满脸牛逼的样子。
