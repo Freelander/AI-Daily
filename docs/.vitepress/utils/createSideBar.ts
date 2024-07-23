6441791cb0910537f1dea905bbcb1f8fccc1949a
Jun <huanggaojun13@gmail.com>
2024-07-23 21:55:54 +0800

    add 7-23 ai daily

--- docs/.vitepress/utils/createSideBar.ts
+++ docs/.vitepress/utils/createSideBar.ts
@@ -4,6 +4,7 @@ export function createSideBarZH() {
       text: '2024 年',
       collapsed: false,
       items: [
+        { text: '7 月 23 日', link: '/posts/2024/7-23' },
         { text: '7 月 22 日', link: '/posts/2024/7-22' },
         { text: '7 月 20 日', link: '/posts/2024/7-20' },
         { text: '7 月 19 日', link: '/posts/2024/7-19' },
@@ -43,6 +44,7 @@ export function createSideBarEN() {
       text: 'The Year 2024',
       collapsed: true,
       items: [
+        { text: 'July 23', link: '/en/posts/2024/7-23' },
         { text: 'July 22', link: '/en/posts/2024/7-22' },
         { text: 'July 21', link: '/en/posts/2024/7-21' },
         { text: 'July 20', link: '/en/posts/2024/7-20' },
